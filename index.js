2d185835a9a613bc6d6ffab2f94e5ae3bdcf1ce5
Niklas Hallberg <91habbe@gmail.com>
2019-02-19 14:07:06 +0100

    installed nodemon. working msg with username, msg and date. added disconnect event

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const io = require('socket.io')(http);
 
 io.on('connection', socket => {
     console.log('a user connected');
-    socket.on('disconnect', function() {
+    
+    socket.on('close', (code, reason) => {
+		console.log('TCL: reason', reason)
+        console.log("code", code);
+    })
+    
+    socket.on('msg', msg => {
+        console.log('message: ', msg);
+        io.emit("msg", msg);
+    });
+
+    socket.on('disconnect', test => {
+        console.log('TCL: test', test)
         console.log('user disconnected');
+        io.emit("userleave", "someone left");
     });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+
+
+// Answer on all http requests
+app.use((req, res) => {
+    console.log("HTTP request on " + req.url);
+    // io.emit("msg", "test");
+    res.send({ msg: "hello" });
+});
+
+http.listen(3002, () => {
+    console.log('listening on *:3002');
 });
